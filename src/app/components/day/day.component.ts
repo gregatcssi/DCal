37833ebe8fb242bae5545617c1fa1cb92d644894
Gregory Mendez <gregory.mendez@groupm.com>
2018-04-25 15:46:19 -0400

    updated to keep it clean

--- src/app/components/day/day.component.ts
+++ src/app/components/day/day.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Day} from '../../iFace/day'
+import {Day} from '../../iFace/day';
 @Component({
   selector: 'app-day',
   templateUrl: './day.component.html',
   styleUrls: ['./day.component.css']
 })
 export class DayComponent implements OnInit {
-  @Input() DispDay:Day;
+  @Input() DispDay: Day;
   constructor() { }
 
   ngOnInit() {
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
-    var newdate = month + "/" + day + "/" + year;
-    var id = "M"+month+"D"+day+"Y"+year;
-    if(this.DispDay.FullDate==newdate){
-      this.DispDay.Color="Blue" 
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1;
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const newdate = month + '/' + day + '/' + year;
+    const id = 'M' + month + 'D' + day + 'Y' + year;
+    if (this.DispDay.FullDate === newdate) {
+      this.DispDay.Color = 'Blue';
     }
   }
 
